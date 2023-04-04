901a2638819945c0da0739fae14166ab78d5d3bb
Oleksander-Kozlov <kozlov.oleksandr89@gmail.com>
2023-04-04 21:11:27 +0300

    Update 01-gallery.js

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -3,3 +3,13 @@ import { galleryItems } from './gallery-items';
 // Change code below this line
 
 console.log(galleryItems);
+const list = document.querySelector('.gallery');
+list.classList.add('js-gallery');
+const cards = galleryItems.map(
+  ({ preview, original, description }) => `<li class="gallery__item">
+   <a class="gallery__link" href="${original}">
+      <img class="gallery__image" src="${preview}" alt="${description}" title = "${description}"/>
+   </a>
+</li>`
+);
+list.insertAdjacentHTML('beforeend', cards.join(''));
