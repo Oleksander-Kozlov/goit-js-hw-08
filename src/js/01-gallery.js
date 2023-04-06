926e150b3beaa3dd9989f9d9b4e1497c2dd76c66
Oleksander-Kozlov <kozlov.oleksandr89@gmail.com>
2023-04-06 06:02:29 +0300

    task 1 and 2 done

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,9 +1,9 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
-
-console.log(galleryItems);
+import 'simplelightbox/dist/simple-lightbox.min.css';
 const list = document.querySelector('.gallery');
+
 list.classList.add('js-gallery');
 const cards = galleryItems.map(
   ({ preview, original, description }) => `<li class="gallery__item">
@@ -13,3 +13,8 @@ const cards = galleryItems.map(
 </li>`
 );
 list.insertAdjacentHTML('beforeend', cards.join(''));
+
+import SimpleLightbox from 'simplelightbox';
+
+let gallery = new SimpleLightbox('.gallery a', { captionDelay: 250 });
+
--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,39 @@
+import Player from "@vimeo/player";
+import throttle from "lodash.throttle";
 
+const timeUpd = JSON.parse(localStorage.getItem('videoplayer_current_time'))??[];
+const player = new Player('vimeo-player');
+
+const onTimeupdate = function (data) {
+   
+  timeUpd.push(data.seconds);
+  localStorage.setItem('videoplayer_current_time', JSON.stringify(timeUpd));
+};
+
+
+ player.on('timeupdate', throttle(onTimeupdate, 1000));
+
+
+
+const currentTime = JSON.parse(
+  localStorage.getItem('videoplayer_current_time')
+);
+const sec = currentTime.pop()??0;
+
+player
+  .setCurrentTime(sec)
+  .then(function (seconds) {
+   
+    // seconds = the actual time that the player seeked to
+  })
+  .catch(function (error) {
+    switch (error.name) {
+      case 'RangeError':
+        // the time was less than 0 or greater than the video’s duration
+        break;
+
+      default:
+        // some other error occurred
+        break;
+    }
+  });
