b6eaa847a21acd08d9ba28c5585e4d2159bc4faa
Oleksander-Kozlov <kozlov.oleksandr89@gmail.com>
2023-04-07 15:12:56 +0300

    Update 03-feedback.js

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,35 @@
+import throttle from 'lodash.throttle';
 
+const form = document.querySelector('.feedback-form');
+console.dir(form.firstElementChild);
+
+const userDataArr = JSON.parse(
+  localStorage.getItem('feedback-form-state')) ?? [];
+
+form.addEventListener('submit', handleSubmit);
+
+function handleSubmit(event) {
+  event.preventDefault();
+
+  const {
+    elements: { email, message },
+  } = event.currentTarget;
+
+  if (email.value.trim() === '' || message.value.trim() === '') {
+    return alert("Шановний(а), будь ласка, обов'язково заповніть всі поля!");
+  } else {
+    const userData = {
+      [email.name]: event.currentTarget.elements.email.value.trim(),
+      [message.name]: event.currentTarget.elements.message.value.trim(),
+    };
+    userDataArr.push(userData);
+    localStorage.setItem('feedback-form-state', JSON.stringify(userDataArr));
+    console.log(userData);
+  }
+//   if (JSON.parse(localStorage.getItem('feedback-form-state'))) {
+//   }
+ const userMail = JSON.parse(localStorage.getItem('feedback-form-state'));
+ form.firstChild.value = userMail.pop().email;
+}
+
+ 
