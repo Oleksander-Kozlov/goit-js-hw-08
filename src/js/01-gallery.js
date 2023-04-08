453369e8aa72dfd53345007154293d9f68668062
Oleksander-Kozlov <kozlov.oleksandr89@gmail.com>
2023-04-08 14:58:01 +0300

    all coment

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,21 +1,21 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
-import SimpleLightbox from 'simplelightbox';
-import 'simplelightbox/dist/simple-lightbox.min.css';
-const list = document.querySelector('.gallery');
-list.style.listStyle = `none`;
-list.classList.add('js-gallery');
-const cards = galleryItems.map(
-  ({ preview, original, description }) => `<li class="gallery__item">
-   <a class="gallery__link" href="${original}">
-      <img class="gallery__image" src="${preview}" alt="${description}" title = "${description}"/>
-   </a>
-</li>`
-);
-list.insertAdjacentHTML('beforeend', cards.join(''));
+// import SimpleLightbox from 'simplelightbox';
+// import 'simplelightbox/dist/simple-lightbox.min.css';
+// const list = document.querySelector('.gallery');
+// list.style.listStyle = `none`;
+// list.classList.add('js-gallery');
+// const cards = galleryItems.map(
+//   ({ preview, original, description }) => `<li class="gallery__item">
+//    <a class="gallery__link" href="${original}">
+//       <img class="gallery__image" src="${preview}" alt="${description}" title = "${description}"/>
+//    </a>
+// </li>`
+// );
+// list.insertAdjacentHTML('beforeend', cards.join(''));
 
 
 
-let gallery = new SimpleLightbox('.gallery a', { captionDelay: 250 });
+// let gallery = new SimpleLightbox('.gallery a', { captionDelay: 250 });
 
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,38 +1,38 @@
-import throttle from 'lodash.throttle';
+// import throttle from 'lodash.throttle';
 
-const form = document.querySelector('.feedback-form');
-const input = form.email;
-const textArea = form.message;
-console.log(input, textArea);
+// const form = document.querySelector('.feedback-form');
+// const input = form.email;
+// const textArea = form.message;
+// console.log(input, textArea);
 
-const userDataArr = JSON.parse(
-  localStorage.getItem('feedback-form-state')) ?? [];
+// const userDataArr = JSON.parse(
+//   localStorage.getItem('feedback-form-state')) ?? [];
 
-form.addEventListener('submit', handleSubmit);
+// form.addEventListener('submit', handleSubmit);
 
-function handleSubmit(event) {
-  event.preventDefault();
+// function handleSubmit(event) {
+//   event.preventDefault();
 
-  const {
-    elements: { email, message },
-  } = event.currentTarget;
+//   const {
+//     elements: { email, message },
+//   } = event.currentTarget;
 
-  if (email.value.trim() === '' || message.value.trim() === '') {
-    return alert("Шановний(а), будь ласка, обов'язково заповніть всі поля!");
-  } else {
-    const userData = {
-      [email.name]: event.currentTarget.elements.email.value.trim(),
-      [message.name]: event.currentTarget.elements.message.value.trim(),
-    };
-    userDataArr.push(userData);
-    localStorage.setItem('feedback-form-state', JSON.stringify(userDataArr));
-    console.log(userData);
-  }
-//   if (JSON.parse(localStorage.getItem('feedback-form-state'))) {
+//   if (email.value.trim() === '' || message.value.trim() === '') {
+//     return alert("Шановний(а), будь ласка, обов'язково заповніть всі поля!");
+//   } else {
+//     const userData = {
+//       [email.name]: event.currentTarget.elements.email.value.trim(),
+//       [message.name]: event.currentTarget.elements.message.value.trim(),
+//     };
+//     userDataArr.push(userData);
+//     localStorage.setItem('feedback-form-state', JSON.stringify(userDataArr));
+//     console.log(userData);
 //   }
+// //   if (JSON.parse(localStorage.getItem('feedback-form-state'))) {
+// //   }
  
- const userMail = JSON.parse(localStorage.getItem('feedback-form-state'));
- form.firstChild.value = userMail.pop().email;
-}
+//  const userMail = JSON.parse(localStorage.getItem('feedback-form-state'));
+//  form.firstChild.value = userMail.pop().email;
+// }
 
  
