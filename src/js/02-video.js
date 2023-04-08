1d029905c808382ef0bd06929d3e03418f9c0008
Oleksander-Kozlov <kozlov.oleksandr89@gmail.com>
2023-04-08 15:25:23 +0300

    fix

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,44 +1,44 @@
-// import Player from "@vimeo/player";
-// import throttle from "lodash.throttle";
+import Player from "@vimeo/player";
+import throttle from "lodash.throttle";
 
-// const timeUpd = JSON.parse(localStorage.getItem('videoplayer_current_time'))??[];
-// const player = new Player('vimeo-player');
+const timeUpd = JSON.parse(localStorage.getItem('videoplayer_current_time'))??[];
+const player = new Player('vimeo-player');
 
-// let userTime = Number
-//   if (JSON.parse(localStorage.getItem('videoplayer_current_time')
-//   )) {
-//     userTime = Number(JSON.parse(
-//       localStorage.getItem('videoplayer_current_time')
-//     ).slice(-1)[0]);
-//   }
-//  else { userTime = 0 } ;
+let userTime = Number
+  if (JSON.parse(localStorage.getItem('videoplayer_current_time')
+  )) {
+    userTime = Number(JSON.parse(
+      localStorage.getItem('videoplayer_current_time')
+    ).slice(-1)[0]);
+  }
+ else { userTime = 0 } ;
 
 
-//     console.log(userTime);
-// const onTimeupdate = function (data) { 
-//   timeUpd.push(data.seconds);
-//   localStorage.setItem('videoplayer_current_time', JSON.stringify(timeUpd)); 
-// };
+    
+const onTimeupdate = function (data) { 
+  timeUpd.push(data.seconds);
+  localStorage.setItem('videoplayer_current_time', JSON.stringify(timeUpd)); 
+};
 
-// player.on('timeupdate', throttle(onTimeupdate, 1000));
+player.on('timeupdate', throttle(onTimeupdate, 1000));
 
 
  
 
 
-// player
-//   .setCurrentTime(userTime)
-//   .then(function (seconds) {
-//     // seconds = the actual time that the player seeked to
-//   })
-//   .catch(function (error) {
-//     switch (error.name) {
-//       case 'RangeError':
-//         // the time was less than 0 or greater than the video’s duration
-//         break;
+player
+  .setCurrentTime(userTime)
+  .then(function (seconds) {
+    // seconds = the actual time that the player seeked to
+  })
+  .catch(function (error) {
+    switch (error.name) {
+      case 'RangeError':
+        // the time was less than 0 or greater than the video’s duration
+        break;
 
-//       default:
-//         // some other error occurred
-//         break;
-//     }
-//   });
+      default:
+        // some other error occurred
+        break;
+    }
+  });
