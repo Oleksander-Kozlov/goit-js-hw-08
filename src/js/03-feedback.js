9eef00ac9a572b8bfb032dff4396dda00ccf5871
Oleksander-Kozlov <kozlov.oleksandr89@gmail.com>
2023-04-12 08:20:01 +0300

    Update 03-feedback.js

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,4 +1,5 @@
 import throttle from 'lodash.throttle';
+import debounce from 'lodash.throttle';
 
 const form = document.querySelector('.feedback-form');
 const input = form.email;
@@ -15,8 +16,25 @@ function onInput(event) {
     email: input.value.trim(),
     message: event.currentTarget.elements.message.value.trim(),
   };
+
   localStorage.setItem('feedback-form-state', JSON.stringify(userData));
 }
+// input.addEventListener('focus', onFocus);
+// textArea.addEventListener('focus', onFocus);
+
+// function onFocus(evt) {
+//   if (JSON.parse(localStorage.getItem('feedback-form-state'))) {
+//     const localData = JSON.parse(
+//       localStorage.getItem('feedback-form-state'))
+//     Object.values(localData);
+//     if (evt.target === input) {
+//       evt.target.value = Object.values(localData)[0];
+
+//     } else {
+//       evt.target.value = Object.values(localData)[1]
+//     }
+//   }
+// }
 
 // Під час завантаження сторінки перевіряй стан сховища, і якщо там є збережені дані, заповнюй ними поля форми. В іншому випадку поля повинні бути порожніми.
 if (JSON.parse(localStorage.getItem('feedback-form-state'))) {
@@ -26,10 +44,10 @@ if (JSON.parse(localStorage.getItem('feedback-form-state'))) {
 }
 
 // Під час сабміту форми очищуй сховище і поля форми, а також виводь у консоль об'єкт з полями email, message та їхніми поточними значеннями.
+
 form.addEventListener('submit', handleSubmit);
 
 function handleSubmit(event) {
-  
   event.preventDefault();
 
   const {
@@ -40,9 +58,10 @@ function handleSubmit(event) {
     return alert("Шановний(а), будь ласка, обов'язково заповніть всі поля!");
   } else {
     localStorage.removeItem('feedback-form-state');
-    console.log(
-      `{email: ${event.currentTarget.elements.email.value.trim()}, message: ${event.currentTarget.elements.message.value.trim()}}`
-    );
+    console.log({
+      email: email.value.trim(),
+      message: message.value.trim(),
+    });
     input.value = '';
     textArea.value = '';
   }
